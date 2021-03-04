fb56512a317e972e82da53c67ac1d9487edef89f
sengarsameer <sengar.sameer@gmail.com>
2021-03-04 20:25:38 +0530

    final commit

---
+++ app.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const app = express();
+require('./src/db/mongoose');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const photoRoutes = require('./src/routes/router');
+
+// Using third-party Middleware - logger & bodyparser
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+// CORS Handeling
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers',
+        'origin, X-Requested-With, Content-Type, Accept, Authorization'
+    )
+    if(req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE');
+        return res.status(200).json({});
+    }
+    next();
+})
+
+// Adding route module which handle request
+app.use('/api', photoRoutes);
+
+// Handle Production
+if(process.env.NODE_ENV == 'production') {
+    //static folder
+    app.use(express.static(__dirname + '/public/'));
+    // Handle SPA
+    app.get(/.*/, (req, res, next) => res.sendFile(__dirname + '/public/index.html'));
+}
+
+// Error Handeling
+app.use((req, res, next) => {
+    const error = new Error('Not Found');
+    error.status = 404;
+    next(error);
+    // next(createError(404));
+})
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+})
+
+module.exports = app;
---
+++ client/src/store/index.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import * as mutations from './mutations'
+import * as actions from './actions'
+import * as getters from './getters'
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+    photos: [],
+    currentPage: 0,
+    allPages: 0,
+    perPage: 0,
+    locationsInfo: [],
+    addLocationsInfo: [],
+    loading: false
+  },
+  actions,
+  mutations,
+  getters
+})
